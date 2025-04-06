ada310b97394996a7c6708b6071f7d192d823da3
Arzoo <115339667+desireeye@users.noreply.github.com>
2025-04-06 19:02:30 +0530

    Add flutter_app/lib/constants.dart 1743946352382

---
+++ flutter_app/lib/constants.dart
@@ -0,0 +1,5 @@
+const String firebaseApiKey = "YOUR_API_KEY";
+const String menuCollectionPath = "menu_items";
+const String ordersCollectionPath = "orders";
+const String chatsCollectionPath = "chats";
+const String usersCollectionPath = "users";
